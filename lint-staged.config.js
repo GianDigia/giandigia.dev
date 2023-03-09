f208cb402f9247c3d9c9a515027fe4b29ea6958f
Gianmarco Digiacomo <gianmarco@at-point.ch>
2023-03-09 20:10:00 +0100

    Configure linter and pre-commit hook

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Type check TypeScript files
+  // Lint then format TypeScript files
+  '**/*.(ts|tsx)': (filenames) => [
+    'yarn tsc --noEmit',
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
+}
